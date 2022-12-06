7b41a76f76632aa473cb7679e9df1b6a058f5221
Olga Martazova <71041400+OlgaMartazova@users.noreply.github.com>
2022-12-06 19:58:44 +0300

    refactoring

--- src/FanoCoder.java
+++ src/FanoCoder.java
@@ -2,52 +2,61 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FanoCoder {
     public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
-        String text = sc.nextLine();
-        String fileName = "input.txt";
-
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-        String[] letters = bufferedReader.readLine().split("\\s");
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("input.txt"));
+        String alphabet = bufferedReader.readLine();
+        List<String> letters = Arrays.stream(alphabet.split("\\s")).collect(Collectors.toList());
         double[] probs = Arrays
                 .stream(bufferedReader.readLine().split("\\s"))
                 .mapToDouble(Double::parseDouble)
                 .toArray();
-        int n = letters.length;
+
+        int n = letters.size();
         List<Node> nodes = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            nodes.add(new Node(letters[i], probs[i], 0, n));
-        }
-
+        IntStream.range(0, n).forEach(i -> nodes.add(new Node(letters.get(i), probs[i], 0, n)));
         sortByProbs(n, nodes);
         fano(0, n - 1, nodes);
-        //printResult(n, nodes);
-        System.out.println(getCode(text, n, nodes));
+
+        printResult(n, nodes);
+
+        System.out.print("Напишите сообщение, состоящее из букв алфавита ");
+        System.out.println(alphabet);
+        String text = sc.nextLine();
+        while (text.isEmpty()) {
+            System.out.println("Сообщение не было получено, попробуйте еще раз");
+            text = sc.nextLine();
+        }
+        for (String ch : text.split("")) {
+            if (!letters.contains(ch)) {
+                System.out.println("В сообщении используются символы, которых нет в алфавите");
+                return;
+            }
+        }
+        System.out.println("Получен код:");
+        System.out.print(getCode(text, n, nodes));
     }
 
 
     private static void fano(int left, int right, List<Node> nodes) {
-        double group1 = 0;
-        double total = 0;
-        double diff1 = 0;
-        if (left == right || left > right) {
+        if (left >= right) {
             return;
         }
         if (right - left == 1) {
-            nodes.get(left).code[nodes.get(left).topPosition] = 0;
-            nodes.get(left).topPosition += 1;
-            nodes.get(right).code[nodes.get(right).topPosition] = 1;
-            nodes.get(right).topPosition += 1;
+            setCode(left, 0, nodes);
+            setCode(right, 1, nodes);
         } else {
+            double total = 0;
             for (int i = left; i <= right; i++) {
                 total += nodes.get(i).probability;
             }
-            group1 = nodes.get(left).probability;
-            diff1 = Math.abs(group1 - total / 2);
+            double group1 = nodes.get(left).probability;
+            double diff1 = Math.abs(group1 - total / 2);
             int board = 1;
             group1 += nodes.get(left + board).probability;
             double diff2 = Math.abs(group1 - total / 2);
@@ -57,36 +66,25 @@ public class FanoCoder {
                 group1 += nodes.get(left + board).probability;
                 diff2 = Math.abs(group1 - total / 2);
             }
-            board--;
-            board = board + left;
-            for (int i = left; i <= board; i++) {
-                nodes.get(i).code[nodes.get(i).topPosition] = 0;
-                nodes.get(i).topPosition += 1;
-            }
-            for (int i = board + 1; i <= right; i++) {
-                nodes.get(i).code[nodes.get(i).topPosition] = 1;
-                nodes.get(i).topPosition += 1;
-            }
+            board += left - 1;
+            IntStream.range(left, board + 1).forEach(i -> setCode(i, 0, nodes));
+            IntStream.range(board + 1, right + 1).forEach(i -> setCode(i, 1, nodes));
             fano(left, board, nodes);
             fano(board + 1, right, nodes);
         }
     }
 
-    private static int sumFromTo(int left, int right, List<Node> nodes) {
-        int sum = 0;
-        for (int i = left; i < right; i++) {
-            sum += nodes.get(i).probability;
-        }
-        return sum;
+    private static void setCode(int i, int code, List<Node> nodes) {
+        nodes.get(i).code[nodes.get(i).topPosition] = code;
+        nodes.get(i).topPosition += 1;
     }
-
 
     private static void sortByProbs(int n, List<Node> nodes) {
         Node current = new Node();
         for (Node node : nodes) {
             for (int i = 0; i < n - 1; i++) {
                 if (nodes.get(i).probability < nodes.get(i + 1).probability) {
-//                swap
+                    //swap
                     current.probability = nodes.get(i).probability;
                     current.symbol = nodes.get(i).symbol;
 
@@ -102,28 +100,22 @@ public class FanoCoder {
 
     private static void printResult(int n, List<Node> nodes) {
         System.out.println("Symbol\tprobability\tcode");
-        for (int i = 0; i < n; i++) {
+        IntStream.range(0, n).forEach(i -> {
             System.out.print(nodes.get(i).symbol + "\t\t" + nodes.get(i).probability + "\t\t");
-            for (int j = 0; j < nodes.get(i).topPosition; j++) {
-                System.out.print(nodes.get(i).code[j]);
-            }
+            IntStream.range(0, nodes.get(i).topPosition).forEach(j -> System.out.print(nodes.get(i).code[j]));
             System.out.println();
-        }
+        });
     }
 
     private static String getCode(String text, int n, List<Node> nodes) {
         Map<String, String> code = new HashMap<>();
-        for (int i = 0; i < n; i++) {
+        IntStream.range(0, n).forEach(i -> {
             StringBuilder codeSeq = new StringBuilder();
-            for (int j = 0; j < nodes.get(i).topPosition; j++) {
-                codeSeq.append(nodes.get(i).code[j]);
-            }
+            IntStream.range(0, nodes.get(i).topPosition).forEach(j -> codeSeq.append(nodes.get(i).code[j]));
             code.put(nodes.get(i).symbol, codeSeq.toString());
-        }
+        });
         StringBuilder output = new StringBuilder();
-        for (char ch : text.toCharArray()) {
-            output.append(code.get(String.valueOf(ch)));
-        }
+        Arrays.stream(text.split("")).forEach(letter -> output.append(code.get(letter)));
         return output.toString();
     }
 }
